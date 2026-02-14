c60e53c449b0e6b1107f5292ce98976ef4d04daa
SrinivaasGudur07 <srinivaasguduru@gmail.com>
2026-02-14 13:39:56 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,18 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user",(req,res)=>{
-    res.send({firstName:"akshay",lastName:"saini"})
+app.use("/user",
+    (req,res,next)=>{
+    //res.send("responding from server 1")
+    console.log("server 1");
+    next();
+},
+    (req,res,next)=>{
+    //res.send("responding from server 2")
+    console.log("server 2");
+    next();
+},
+    (req,res,next)=>{
+    //res.send("response from sever 3")
+    console.log("server 3")
+    next()
+},
+    (req,res,next)=>{
+    //res.send("respinse from server 4")
+    console.log("server 4")
+    next();
+},
+    (req,res,next)=>{
+    res.send("response 5")
+    
 })
 
-app.post("/user",(req,res)=>{
-    //saving data to DB
-    res.send("Data succesfully saved to the DataBase!")
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("data deledted successfully!!!")
-})
 
 app.listen(3000,()=>{
     console.log("server is running successfully!!!")
