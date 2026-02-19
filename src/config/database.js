00a6b1916a849af3707ea0c0a9ba6191cda60029
SrinivaasGudur07 <srinivaasguduru@gmail.com>
2026-02-19 12:31:04 +0530

    added

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const connectDB = async ()=>{
+    await mongoose.connect(
+        "mongodb+srv://Admin:FWeMsq5U0S0tzaSv@namastenode.vb705sw.mongodb.net/devTinder"
+    );
+};
+
+module.exports = connectDB;
+
+/*
+const mongoose= require("mongoose")
+
+const connectDB =async ()=>{
+    await mongoose.connect("URL")
+    }
+ */
+
